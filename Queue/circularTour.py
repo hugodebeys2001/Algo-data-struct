a2841b79af1c06f4bbb62ad28b03295c97848b3e
hugodebeys2001 <150696017+hugodebeys2001@users.noreply.github.com>
2024-06-01 20:30:36 +0200

    Update circularTour.py

--- Queue/circularTour.py
+++ Queue/circularTour.py
@@ -1,26 +1,31 @@
+#https://www.geeksforgeeks.org/problems/circular-tour-1587115620/1
+from collections import deque
+
 class Solution:
 
     # Function to find starting point where the truck can start to get through
     # the complete circle without exhausting its petrol in between.
     def tour(self, lis, n):
+        queue = deque()
         total_petrol = 0
-        start_index = 0
         current_petrol = 0
+        start_index = 0
 
         for i in range(n):
             petrol, distance = lis[i]
+            queue.append((petrol, distance))
             total_petrol += petrol - distance
             current_petrol += petrol - distance
 
-            # Si la quantité de carburant devient négative, cela signifie que le camion ne peut pas atteindre
-            # la station-service suivante en partant de la station actuelle
+            # If current petrol becomes negative, meaning the truck can't reach the next petrol pump from the current one
             if current_petrol < 0:
-                # Réinitialiser la station de départ à la suivante
+                # Reset start_index to the next petrol pump
                 start_index = i + 1
                 current_petrol = 0
+                queue.clear()  # Clear the queue since we are resetting the starting point
 
-        # Si le total du carburant disponible est positif, cela signifie qu'il existe une solution
-        # Sinon, il n'y a pas de solution
+        # If total available petrol is non-negative, there is a solution
+        # Otherwise, there is no solution
         if total_petrol >= 0:
             return start_index
         else:
