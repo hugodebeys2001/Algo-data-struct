1675e48fcf9c9115931b8465bf0948f9595d90d9
hugodebeys2001 <150696017+hugodebeys2001@users.noreply.github.com>
2024-06-02 17:18:57 +0200

    Update Design twitter.py

--- Hash/Design twitter.py
+++ Hash/Design twitter.py
@@ -1,3 +1,5 @@
+#https://www.geeksforgeeks.org/problems/design-twitter/0
+
 from collections import defaultdict
 from heapq import merge
 
@@ -7,17 +9,17 @@ class Tweet:
         self.tweetId = tweetId
         self.timestamp = timestamp
 
-    # Define comparison method
+    # Defini la éthode de comparison
     def __lt__(self, other):
         return self.timestamp < other.timestamp
 
 
 class Twitter:
     def __init__(self):
-        self.tweets = defaultdict(list)  # User ID -> List of Tweets
-        self.followees = defaultdict(set)  # User ID -> Set of Followees
+        self.tweets = defaultdict(list)  # User ID -> Liste des Tweets
+        self.followees = defaultdict(set)  # User ID -> Sets des Followees
 
-        # Timestamp for tweets
+
         self.timestamp = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
@@ -25,12 +27,29 @@ class Twitter:
         self.tweets[userId].append(Tweet(tweetId, self.timestamp))
 
     def getNewsFeed(self, userId: int):
-        # Merge tweets from user and followees
-        tweets = merge(*[self.tweets[u] for u in (userId, *self.followees[userId])])
+        # Récupérer les tweets de l'utilisateur
+        user_tweets = self.tweets[userId]
 
-        # Reverse the order of tweets and select the first 10
-        tweets = reversed(list(tweets))
-        return [tweet.tweetId for _, tweet in zip(range(10), tweets)]
+        # Récupérer les tweets de chaque followee
+        followees_tweets = [self.tweets[u] for u in self.followees[userId]]
+
+        all_tweets = [user_tweets] + followees_tweets
+
+        # Fusionner toutes les listes de tweets triés
+        merged_tweets = merge(*all_tweets)
+
+        # Inverser l'ordre des tweets
+        tweets = reversed(list(merged_tweets))
+
+        # Créer une liste vide pour les identifiants des tweets
+        tweet_ids = []
+
+        # Utiliser zip pour itérer simultanément sur les indices et les tweets
+        for _, tweet in zip(range(10), tweets):
+            tweet_ids.append(tweet.tweetId)
+
+        # Retourner la liste des identifiants des tweets
+        return tweet_ids
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId != followeeId:
