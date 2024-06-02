ca49dd4ff0a486d4b552b417ee486a622d34fef8
hugodebeys2001 <150696017+hugodebeys2001@users.noreply.github.com>
2024-06-02 11:23:21 +0200

    Update Sort a stack.py

--- Stack/Sort a stack.py
+++ Stack/Sort a stack.py
@@ -1,26 +1,23 @@
 #https://www.geeksforgeeks.org/problems/sort-a-stack/1
 
 class Solution:
-    def trier_pile(self, pile):
+    def Sorted(self, stack):
         # Pile auxiliaire pour stocker les éléments temporairement
-        pile_auxiliaire = []
+        aux_stack = []
 
         # Tant que la pile principale n'est pas vide
-        while pile:
+        while stack:
             # Retirer le dernier élément de la pile principale
-            element_temporaire = pile.pop()
-
+            temp = stack.pop()
             # Déplacer les éléments de la pile auxiliaire vers la pile principale
             # jusqu'à ce que le sommet de la pile auxiliaire soit plus petit que l'élément actuel
-            while pile_auxiliaire and pile_auxiliaire[-1] < element_temporaire:
-                pile.append(pile_auxiliaire.pop())
+            while aux_stack and aux_stack[-1] < temp:
+                stack.append(aux_stack.pop())
 
             # Ajouter l'élément actuel à la pile auxiliaire
-            pile_auxiliaire.append(element_temporaire)
-
+            aux_stack.append(temp)
         # À ce stade, la pile auxiliaire contient les éléments triés par ordre décroissant
         # Déplacer les éléments de la pile auxiliaire vers la pile principale pour les trié par ordre croissant
-        while pile_auxiliaire:
-            pile.append(pile_auxiliaire.pop())
-
-        #Dans leur code, il réinverse une fois de plus la pile.
+        while aux_stack:
+            stack.append(aux_stack.pop())
+# Dans leur code, il réinverse une fois de plus la pile.
