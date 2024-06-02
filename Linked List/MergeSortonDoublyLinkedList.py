6d1bf17d3389f06fc16c885bf9bf76a61757dc56
hugodebeys2001 <150696017+hugodebeys2001@users.noreply.github.com>
2024-06-02 10:04:33 +0200

    Update MergeSortonDoublyLinkedList.py

--- Linked List/MergeSortonDoublyLinkedList.py
+++ Linked List/MergeSortonDoublyLinkedList.py
@@ -1,63 +1,63 @@
 #https://www.geeksforgeeks.org/problems/merge-sort-on-doubly-linked-list/1
-#Function to merge two halves of list.
+# Fonction pour fusionner deux moitiés de la liste.
 def merge(first, second):
-    #base cases when either of two halves is null.
-    if first is None: 
-        return second  
-    if second is None: 
-        return first 
+    # Cas de base où l'une des deux moitiés est nulle.
+    if first is None:
+        return second
+    if second is None:
+        return first
 
-    #comparing data in both halves and storing the smaller in result and
-    #recursively calling the merge function for next node in result.
-    if first.data < second.data: 
-        first.next = merge(first.next, second) 
-        first.next.prev = first 
+        # Comparaison des données dans les deux moitiés et stockage du plus petit dans le résultat.
+    # Appel récursif de la fonction de fusion pour le nœud suivant dans le résultat.
+    if first.data < second.data:
+        first.next = merge(first.next, second)
+        first.next.prev = first
         first.prev = None
-        #returning the resultant list.
-        return first 
-    else: 
-        second.next = merge(first, second.next) 
-        second.next.prev = second 
+        # Retourne la liste résultante.
+        return first
+    else:
+        second.next = merge(first, second.next)
+        second.next.prev = second
         second.prev = None
-        #returning the resultant list.
-        return second 
+        # Retourne la liste résultante.
+        return second
 
-#Function to sort the given doubly linked list using Merge Sort. 
-def sortDoubly(head): 
-    if head is None:  
-        return head 
-    if head.next is None: 
-        return head 
-    
-    #splitting the list into two halves. 
-    second = split(head) 
-      
-    #calling the sortDoubly function recursively for both parts separately. 
-    head = sortDoubly(head) 
-    second = sortDoubly(second) 
+    # Fonction pour trier la liste doublement chaînée donnée en utilisant le tri par fusion.
 
-    #calling the function to merge both halves.
-    return merge(head, second) 
 
-#Function to split the list into two halves.
+def sortDoubly(head):
+    if head is None:
+        return head
+    if head.next is None:
+        return head
+
+        # Division de la liste en deux moitiés.
+    second = split(head)
+
+    # Appel récursif de la fonction sortDoubly pour chaque moitié séparément.
+    head = sortDoubly(head)
+    second = sortDoubly(second)
+
+    # Appel de la fonction pour fusionner les deux moitiés.
+    return merge(head, second)
+
+
+# Fonction pour diviser la liste en deux moitiés.
 def split(head):
-    
-    #using two pointers to find the midpoint of list.
-    fast = slow =  head 
-    
-    #first pointer, slow moves 1 node and second pointer, fast moves
-    #2 nodes in one iteration.
-    while(True): 
-        if fast.next is None: 
+    # Utilisation de deux pointeurs pour trouver le point milieu de la liste.
+    fast = slow = head
+
+    # Le premier pointeur (slow) avance d'un nœud et le second pointeur (fast) avance
+    # de deux nœuds à chaque itération.
+    while (True):
+        if fast.next is None:
             break
-        if fast.next.next is None: 
+        if fast.next.next is None:
             break
-        fast = fast.next.next 
+        fast = fast.next.next
         slow = slow.next
-    
-    #slow is before the midpoint in the list, so we split the 
-    #list in two halves from that point.      
+
+    # slow est avant le milieu de la liste, donc on divise la liste en deux moitiés à partir de ce point.
     temp = slow.next
     slow.next = None
-    return temp 
-    
+    return temp
